85e070ae2801b8d4a9a02e384b9ede3e21d121d9
Mike Kim <mikekim.info@gmail.com>
2022-11-29 15:41:27 -0500

    resolves gh-13; clean up redundant state, create basic validation

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import guessesReducer from './guesses';
 import rowIndexReducer from './rowIndex';
+import { solutionReducer } from './solution';
 
 const appReducer = combineReducers({
   guesses: guessesReducer,
   rowIndex: rowIndexReducer,
+  solution: solutionReducer,
 });
 
 export default appReducer;
